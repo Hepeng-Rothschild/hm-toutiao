24528ef0c75ec0cbdafc372e803a29c40ef43a36
magicstf <745555655@qq.com>
2019-08-11 13:42:03 +0800

    登录功能完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
+// 导入样式
+import '@/styles/index.less'
+import axios from 'axios'
+Vue.prototype.$http = axios
+// import router from './router/index.js'
+// @是一个路径的别名   是src路径   只有在vue-cli项目下有效
+// index是目录的索引文件  默认使用的索引文件 index。js index.vue
 // 产品级别提示   不开启  开发阶段 提示越详细越好  开发级别提示
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 // main.js 入口文件
 // 职责1：引入需要依赖的模块（js，包，css，其他资源）
 // 职责2：创建一个根vue实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// 职责：初始化路由对象
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import Login from '@/views/login'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [{
+    // 跳转路由 $router.push('/login') 或者 $router.push({name:'login'})
+    path: '/login',
+    name: 'login',
+    component: Login
+  }]
+})
+export default router
